c06ba95c06c2bb68dec56371dfd88e17576c63d6
Alex Windett <alex@bigfish.co.uk>
2015-10-30 17:25:30 +0000

    working on project page and fixes

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -5,24 +5,27 @@ $(document).ready(function(){
     // ++++++++++++++++++++++++++++++++
     $('.nav__main').clone().appendTo('nav.desktop-nav').removeClass('mobile').addClass('desktop');
 
+    navWidth = $('.nav__main.desktop').width();
+
+    $('.desktop-nav .trigger').css({
+        marginLeft : -Math.abs(navWidth)
+    });
+
     $('.desktop-nav .trigger').click(function(event) {
-        var navWidth = $('.nav__main.desktop').width();
         var effect = 'slide';
-        var duration = 300;
-        console.log(navWidth)
+        var duration = 350;
 
-        event.preventDefault();
-        $(this).toggleClass('triggered');
+        $(this).toggleClass('open');
 
-        if ( $(this).hasClass('triggered') ) {
-            $('.nav__main.desktop').toggle(effect, 'left', duration);
-            $(this).animate({
-                'marginLeft': '-220px',
-            });
-        } else {
+        if ( $(this).hasClass('open') ) {
             $('.nav__main.desktop').toggle(effect, 'right', duration);
             $(this).animate({
-                'marginLeft': 0,
+                'marginLeft': '0',
+            });
+        } else {
+            $('.nav__main.desktop').toggle(effect, 'left', duration);
+            $(this).animate({
+                'marginLeft': -Math.abs(navWidth),
             });
         }
     });
@@ -31,12 +34,42 @@ $(document).ready(function(){
     // ++++++++++++++++++++++++++++++++
     // Home Projects
     // ++++++++++++++++++++++++++++++++
-	$('.projects article').hide();
+    if ( $('body.page-home').length > 0 ) {
+    	$('.projects article').hide();
 
-    $('.item-wrap').mouseenter(function(){
-    	$(this).children('article').show();
-    }).mouseleave(function(){
-    	$(this).children('article').hide();
-    })
+        $('.item-wrap').mouseenter(function(){
+        	$(this).children('article').show();
+        }).mouseleave(function(){
+        	$(this).children('article').hide();
+        })
+    }
 
+    // ++++++++++++++++++++++++++++++++
+    // Project Page
+    // ++++++++++++++++++++++++++++++++
+    if ( $('body.page-single-project').length > 0 ) {
+
+        // For each of the lightbox images clone them into
+        // Foundation reveal
+        $('.image__wrapper').each(function(){
+            var img = $(this).find('img');
+
+            img.clone()
+                .appendTo($(this).find('.reveal-modal'))
+                .addClass('hi_res');
+        })
+
+        // Change the ending of the file source to be the
+        // Hi-Res image
+        $('img.hi_res').attr('src', function(index, attr){
+            return attr.replace('.png', '_hi-res.png');
+        });
+    }
+
+    // ++++++++++++++++++++++++++++++++
+    // General Helpers
+    // ++++++++++++++++++++++++++++++++
+    $('a.link__removed').click(function(event) {
+        event.preventDefault();
+    });
 });
